4309974fbcb7f0ec5f20290ca34453172425bdbe
Marvin Herbold <marvin.herbold@gmail.com>
2018-09-10 08:28:18 -0700

    New transporter sound and improve floor height transition

--- Assets/Scenes/Starport/Scripts/AstronautController.cs
+++ Assets/Scenes/Starport/Scripts/AstronautController.cs
@@ -4,26 +4,11 @@ using UnityEngine.AI;
 
 public class AstronautController : MonoBehaviour
 {
-	// the transporter particle system
-	public ParticleSystem m_transporterParticleSystem;
-
 	// the navmesh agent (used for controlling the astronaut)
 	public NavMeshAgent m_navMeshAgent;
 
-	// the animator (used for making him run or be bored)
+	// the animator (used for making him run or be idle)
 	public Animator m_animator;
-
-	// unity awake
-	void Awake()
-	{
-	}
-
-	// this is called by unity every frame
-	void Update()
-	{
-		// move the transporter particles along with the astronaut
-		m_transporterParticleSystem.transform.position = transform.position;
-	}
 
 	// call this to move the astronaut
 	public void Move( Vector3 relativePosition )
