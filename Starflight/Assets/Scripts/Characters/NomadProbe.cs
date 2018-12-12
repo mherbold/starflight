93cf247152ade153f6ee533cfeaeeba997373038
Marvin Herbold <marvin.herbold@gmail.com>
2018-12-11 19:35:45 -0700

    Nomad Probe Goodness

---
+++ Starflight/Assets/Scripts/Characters/NomadProbe.cs
@@ -0,0 +1,71 @@
+﻿
+using UnityEngine;
+
+public class NomadProbe : MonoBehaviour
+{
+	public float m_blinkInterval1 = 3.5f;
+	public float m_blinkInterval2 = 0.5f;
+	public float m_blinkDuration = 0.25f;
+
+	public GameObject[] m_lights;
+
+	bool m_pingPong;
+	float m_timer;
+
+	void Start()
+	{
+	}
+
+	void Update()
+	{
+		m_timer += Time.deltaTime;
+
+		var lightsOn = false;
+
+		if ( m_pingPong )
+		{
+			if ( m_timer >= m_blinkInterval2 )
+			{
+				if ( m_timer >= m_blinkInterval2 + m_blinkDuration )
+				{
+					m_timer -= m_blinkInterval2 + m_blinkDuration;
+
+					m_pingPong = false;
+				}
+				else
+				{
+					lightsOn = true;
+				}
+			}
+		}
+		else
+		{
+			if ( m_timer >= m_blinkInterval1 )
+			{
+				if ( m_timer >= m_blinkInterval1 + m_blinkDuration )
+				{
+					m_timer -= m_blinkInterval1 + m_blinkDuration;
+
+					m_pingPong = true;
+				}
+				else
+				{
+					lightsOn = true;
+				}
+			}
+		}
+
+		SetActive( lightsOn );
+	}
+
+	void SetActive( bool active )
+	{
+		foreach ( var light in m_lights )
+		{
+			if ( light.activeInHierarchy != active )
+			{
+				light.SetActive( active );
+			}
+		}
+	}
+}
---
+++ Starflight/Assets/Scripts/Misc/ConstantRotation.cs
@@ -0,0 +1,37 @@
+﻿
+using UnityEngine;
+
+public class ConstantRotation : MonoBehaviour
+{
+	public Vector3 m_rotationSpeed;
+
+	Vector3 m_currentRotation;
+	Quaternion m_originalRotation;
+
+	void Start()
+	{
+		m_originalRotation = transform.localRotation;
+	}
+
+	void Update()
+	{
+		m_currentRotation += m_rotationSpeed * Time.deltaTime;
+
+		if ( m_currentRotation.x >= 360.0f )
+		{
+			m_currentRotation.x -= 360.0f;
+		}
+
+		if ( m_currentRotation.y >= 360.0f )
+		{
+			m_currentRotation.y -= 360.0f;
+		}
+
+		if ( m_currentRotation.z >= 360.0f )
+		{
+			m_currentRotation.z -= 360.0f;
+		}
+
+		transform.localRotation = m_originalRotation * Quaternion.Euler( m_currentRotation.x, m_currentRotation.y, m_currentRotation.z );
+	}
+}
