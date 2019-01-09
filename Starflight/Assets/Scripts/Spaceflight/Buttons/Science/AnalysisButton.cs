099747c18125643d650040e227606d8213487f3d
Marvin Herbold <marvin.herbold@gmail.com>
2019-01-09 13:53:34 -0700

    Updated analysis data

--- Starflight/Assets/Scripts/Spaceflight/Buttons/Science/AnalysisButton.cs
+++ Starflight/Assets/Scripts/Spaceflight/Buttons/Science/AnalysisButton.cs
@@ -23,49 +23,71 @@ public class AnalysisButton : ShipButton
 			// yes - start building the new messages text
 			text = "<color=white>Analysis of Last Sensor Reading</color>\n";
 
-			// did we just scan a ship?
-			if ( m_spaceflightController.m_displayController.m_sensorsDisplay.m_scanType <= SensorsDisplay.ScanType.NoahTransport )
+			// what did we just scan
+			switch ( m_spaceflightController.m_displayController.m_sensorsDisplay.m_scanType )
 			{
-				// yes - display the ship information
-				var vessel = gameData.m_vesselList[ (int) m_spaceflightController.m_displayController.m_sensorsDisplay.m_scanType ];
+				case SensorsDisplay.ScanType.Planet:
+				{
+					// get to the planet
+					GD_Planet planet = gameData.m_planetList[ playerData.m_general.m_currentPlanetId ];
 
-				// object
-				text += "Object: <color=white>Vessel</color>\n";
+					// object and orbit number
+					text += "Object: <color=white>Planet</color>   Orbit Number: <color=white>" + planet.m_orbitPosition + "</color>\n";
 
-				// type
-				text += "Type: <color=white>" + vessel.m_type + "</color>\n";
+					// surface
+					text += "Predominant Surface: <color=white>" + planet.GetSurfaceText() + "</color>\n";
 
-				// size
-				text += "Size: <color=white>" + vessel.m_relativeSize + " times the size of our ship</color>\n";
+					// gravity
+					text += "Gravity: <color=white>" + planet.GetGravityText() + "</color>\n";
 
-				// shields
-				text += "Shields: <color=yellow>Not certain</color>\n";
+					// atmospheric density
+					text += "Atmospheric Density: <color=white>" + planet.GetAtmosphericDensityText() + "</color>\n";
 
-				// weapon status
-				text += "Weapon Status: <color=yellow>Not certain</color>\n";
-			}
-			else if ( m_spaceflightController.m_displayController.m_sensorsDisplay.m_scanType == SensorsDisplay.ScanType.Planet )
-			{
-				// yes - get to the planet
-				GD_Planet planet = gameData.m_planetList[ playerData.m_general.m_currentPlanetId ];
+					// temperature
+					text += "Temperature: <color=white>" + planet.GetTemperatureText() + "</color>\n";
 
-				// object and orbit number
-				text += "Object: <color=white>Planet</color>   Orbit Number: <color=white>" + planet.m_orbitPosition + "</color>\n";
+					// weather
+					text += "Global Weather: <color=white>" + planet.GetGlobalWeatherText() + "</color>";
 
-				// surface
-				text += "Predominant Surface: <color=white>" + planet.GetSurfaceText() + "</color>\n";
+					break;
+				}
 
-				// gravity
-				text += "Gravity: <color=white>" + planet.GetGravityText() + "</color>\n";
+				case SensorsDisplay.ScanType.Minstrel:
+				{
+					// display the ship information
+					var vessel = gameData.m_vesselList[ (int) m_spaceflightController.m_displayController.m_sensorsDisplay.m_scanType ];
 
-				// atmospheric density
-				text += "Atmospheric Density: <color=white>" + planet.GetAtmosphericDensityText() + "</color>\n";
+					// object
+					text += "Object: <color=white>" + vessel.m_object + "</color>\n";
 
-				// temperature
-				text += "Temperature: <color=white>" + planet.GetTemperatureText() + "</color>\n";
+					// size
+					text += "Size: <color=white>" + vessel.m_relativeSize + " times the size of our ship</color>\n";
 
-				// weather
-				text += "Global Weather: <color=white>" + planet.GetGlobalWeatherText() + "</color>";
+					break;
+				}
+
+				default:
+				{
+					// display the ship information
+					var vessel = gameData.m_vesselList[ (int) m_spaceflightController.m_displayController.m_sensorsDisplay.m_scanType ];
+
+					// object
+					text += "Object: <color=white>" + vessel.m_object + "</color>\n";
+
+					// type
+					text += "Type: <color=white>" + vessel.m_type + "</color>\n";
+
+					// size
+					text += "Size: <color=white>" + vessel.m_relativeSize + " times the size of our ship</color>\n";
+
+					// shields
+					text += "Shields: <color=yellow>Not certain</color>\n";
+
+					// weapon status
+					text += "Weapon Status: <color=yellow>Not certain</color>\n";
+
+					break;
+				}
 			}
 		}
 		else
