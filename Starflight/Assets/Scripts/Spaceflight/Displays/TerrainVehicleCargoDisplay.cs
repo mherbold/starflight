8f817886b3ebb843f52bbbe9ed492861af810c94
Marvin Herbold <marvin.herbold@gmail.com>
2021-01-20 06:20:22 -0700

    Create TerrainVehicleCargoDisplay.cs

---
+++ Starflight/Assets/Scripts/Spaceflight/Displays/TerrainVehicleCargoDisplay.cs
@@ -0,0 +1,26 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TerrainVehicleCargoDisplay : ShipDisplay
+{
+	TerrainVehicleCargoDisplay()
+	{
+	}
+
+	// the display label
+	public override string GetLabel()
+	{
+		return "Cargo";
+	}
+
+	public override void Show()
+	{
+		base.Show();
+	}
+
+	public override void Update()
+	{
+	}
+}
